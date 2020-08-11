e3f161010bf222e2367a16ba3034b3eb7a48fcd1
AlikanakelaKarwowski <akk42@zips.uakron.edu>
2020-08-10 21:01:03 -0400

    Create simple Rectified Linear function

--- p5.py
+++ p5.py
@@ -4,8 +4,18 @@ X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
 
-class Layer_Dense:
+for i in inputs:
+    if i > 0:
+        output.append(i)
+    elif i <= 0:
+        output.append(0)
+
+print(output)
+
+"""class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
@@ -19,6 +29,4 @@ layer2 = Layer_Dense(5, 2)
 layer1.forward(X)
 print(layer1.output)
 layer2.forward(layer1.output)
-print(layer2.output)
-
-
+print(layer2.output)"""
