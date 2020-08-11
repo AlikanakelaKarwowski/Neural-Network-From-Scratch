02b81f32ee977a0477dcd34300a1295867c59bae
AlikanakelaKarwowski <akk42@zips.uakron.edu>
2020-08-10 20:32:24 -0400

    Create weights and biases in class. Define forward method

--- p4.py
+++ p4.py
@@ -6,26 +6,13 @@ X = [[1, 2, 3, 2.5],
 
 
 class Layer_Dense:
-    def __init__(self):
-        pass
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-    def forward(self):
-        pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
 
-biases = [2.0, 3.0, 0.5]
 
-weights2 = [[0.1, -0.14, 0.5],
-            [-0.5, 0.12, -0.33],
-            [-0.44, 0.73, -0.13]]
-
-biases2 = [-1, 2, -0.5]
-
-layer1_outputs = np.dot(X, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-
-print(layer2_outputs)
