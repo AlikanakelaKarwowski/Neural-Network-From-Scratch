f585d0e468dfaba6f7c596704f0c39cd53416267
AlikanakelaKarwowski <akk42@zips.uakron.edu>
2020-08-10 21:21:54 -0400

    Delete 2nd data layer. Add in spiral Data function from nnfs.

--- p5.py
+++ p5.py
@@ -1,8 +1,13 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
 
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -17,9 +22,6 @@ class Activation_ReLU:
         self.output = np.maximum(0,inputs)
 
 layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+
 
 layer1.forward(X)
-print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
