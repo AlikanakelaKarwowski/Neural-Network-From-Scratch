bcddd105e3dee3a33bc7e5620bbae5ff6d0f7f96
A.Karwowski <akk42@zips.uakron.edu>
2021-03-11 12:46:03 -0500

    update p5 to match book

--- p5.py
+++ p5.py
@@ -1,5 +1,6 @@
 import numpy as np
 import nnfs
+import matplotlib.pyplot as plt
 from nnfs.datasets import spiral_data
 
 nnfs.init()
@@ -9,11 +10,9 @@ X = [[1, 2, 3, 2.5],
      [-1.5, 2.7, 3.3, -0.8]]
 
 X, y = spiral_data(100, 3)
-
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
@@ -25,10 +24,9 @@ class Activation_ReLU:
         self.output = np.maximum(0, inputs)
 
 
-layer1 = Layer_Dense(2, 5)
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-#print(layer1.output)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+print(activation1.output[:5])
