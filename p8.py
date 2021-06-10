33e94231799d8fdcb81f809f67260f3660bc4f64
A.Karwowski <akk42@zips.uakron.edu>
2021-06-10 12:19:20 -0400

    create Loss sub class

--- p8.py
+++ p8.py
@@ -32,6 +32,18 @@ class Loss:
         data_loss = np.mean(sample_losses)
         return data_loss
     
+class  Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred,1e-7, 1-1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+        
+        negative_log_likelihood = -np.log(correct_confidences)
+        return negative_log_likelihood
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
